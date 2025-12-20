f6c13fb26f1627acf15507c7736a3a6c7d19f434
Ben Kuper <bkuperberg@gmail.com>
2025-12-20 16:57:20 +0100

    fix static build

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,26 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: "index.html",
-			precompress: false,
-			strict: false
-		})
-	}
+    kit: {
+        paths: {
+            base: process.env.BASE_PATH ?? '',
+            relative: true
+        },
+
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: false
+        })
+    }
 };
+
+if (!process.env.BASE_PATH && process.env.GITHUB_ACTIONS === 'true' && process.env.GITHUB_REPOSITORY) {
+    config.kit.paths.base = `/${process.env.GITHUB_REPOSITORY.split('/').pop()}`;
+}
 
 export default config;
