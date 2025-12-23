fc7e1168be527f4b38b09d912cd298f94a760eed
Ben Kuper <bkuperberg@gmail.com>
2025-12-23 17:09:49 +0100

    slider mid-zero support

--- src/lib/components/Slider.svelte
+++ src/lib/components/Slider.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	let {
 		value = $bindable(),
 		min,
@@ -16,6 +14,7 @@
 		fgColor = 'var(--slider-fg)'
 	} = $props();
 
+	$inspect(min, max);
 	let infiniteMode = $derived(min === undefined || max === undefined);
 	let sliderDiv = $state(null as HTMLDivElement | null);
 	let sliderWidth = $derived(infiniteMode ? 100 : sliderDiv!.getBoundingClientRect().width);
@@ -26,6 +25,28 @@
 	let isHorizontal = $derived(orientation === 'horizontal');
 
 	let isDragging = $derived(false);
+
+	let midZero = $derived(min < 0 && max > 0);
+	let relativeValue = $derived(
+		midZero ? (value >= 0 ? value / max : value / min) : (value - min) / (max - min)
+	);
+	let relativeZero = $derived(midZero ? Math.abs(min) / (max - min) : 0);
+	let relativeMax = $derived(1 - relativeZero);
+
+	let targetLeft = $derived(
+		!midZero ? 0 : value >= 0 ? relativeZero : relativeZero - relativeValue
+	);
+
+	let targetRight = $derived(
+		1 -
+			(!midZero
+				? relativeValue
+				: value >= 0
+					? relativeZero + relativeValue * relativeMax
+					: relativeZero)
+	);
+
+	$inspect(midZero, targetLeft, targetRight);
 
 	function startDrag(e: MouseEvent) {
 		isDragging = true;
@@ -45,22 +66,23 @@
 		const delta = isHorizontal ? e.movementX : e.movementY;
 		const range = max - min;
 
+		let alteredSensitivity = e.altKey
+			? sensitivity / 10
+			: e.shiftKey
+				? sensitivity * 10
+				: sensitivity;
+
 		if (step > 0) {
 			const stepCount = range / step;
-			const pixelsPerStep = sliderWidth / stepCount / sensitivity;
+			const pixelsPerStep = sliderWidth / stepCount / alteredSensitivity;
 			const stepsMoved = Math.round(delta / pixelsPerStep);
 			value = Math.min(max, Math.max(min, valueAtDown + stepsMoved * step));
 		} else if (!infiniteMode) {
-			const percentDelta = (delta / sliderWidth) * sensitivity;
+			const percentDelta = (delta / sliderWidth) * alteredSensitivity;
 			value = Math.min(max, Math.max(min, valueAtDown + percentDelta * range));
 		} else //infinite mode, no range
 		{
-			let alteredSensitivity = e.altKey
-				? sensitivity / 100
-				: e.shiftKey
-					? sensitivity
-					: sensitivity / 10;
-			const valueDelta = delta * alteredSensitivity;
+			const valueDelta = (delta * alteredSensitivity) / 10;
 			value = valueAtDown + valueDelta;
 		}
 
@@ -91,44 +113,58 @@
 		onmousedown={startDrag}
 		style="--bg-color: {bgColor}; --fg-color: {fgColor}"
 	>
-		<div class="slider-foreground" style="--value: {(value - min) / (max - min)}"></div>
+		<div
+			class="slider-foreground {midZero ? 'mid-zero' : ''}"
+			style="--value: {(value - min) / (max - min)};  --left:{targetLeft *
+				100}%; --right:{targetRight * 100}%"
+		></div>
 	</div>
 {/if}
 
 <style>
 	.slider {
+		min-width: 6rem;
 		position: relative;
-		min-width: 100px;
 		height: 1rem;
 		user-select: none;
 		border-radius: 0.5rem;
 		overflow: hidden;
 		background: var(--bg-color);
+		border: none;
 	}
 
 	.slider-foreground {
 		position: absolute;
-		left: 0;
+		padding: 0;
+		margin: 0;
+		left: var(--left);
+		right: var(--right);
 		top: 0;
 		height: 100%;
-		border-radius: 12px 0 0 12px;
+		/* border-radius: 12px 0 0 12px; */
 		background: var(--fg-color);
-		width: calc(var(--value) * 100%);
+		/* width: calc(var(--value) * 100%); */
 		pointer-events: none;
 	}
+
+	.slider-foreground.mid-zero {
+		min-width: 1px;
+	}
+
 	/* Hide cursor globally when dragging */
 	:global(.slider-hide-cursor) {
 		cursor: none !important;
 	}
 
 	.infinite-slider {
-		background:none;
+		background: none;
 		border: none;
 		font-size: 0.6rem;
 		cursor: ew-resize;
 	}
 
-	.infinite-slider:hover, .infinite-slider.dragging {
+	.infinite-slider:hover,
+	.infinite-slider.dragging {
 		background-color: rgba(from var(--panel-bg-color) r g b / 10%);
 	}
 </style>
